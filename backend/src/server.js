b345a3ab4069270e021a8c45d47769afb979c6dc
Aryan Jain Chympa <aryanjjchympa@gmail.com>
2025-08-18 11:35:30 +0530

    initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,38 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js"
 import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
-const app=express();
 
-app.use(cors({
-    origin : "http://localhost:5173",
-}));
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 app.use(express.json());
 app.use(rateLimiter)
 
-app.use("/api/notes",notesRoutes)
+app.use("/api/notes", notesRoutes)
 
-    
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
-const PORT=process.env.PORT || 5001;
-
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`Server is running on PORT :${PORT}`);  
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT :${PORT}`);
     })
 })
