25a8f0d365c226866bfb6857496380ad91c87504
Aryan Jain Chympa <aryanjjchympa@gmail.com>
2025-10-25 20:55:18 +0530

    first commit

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,60 +3,62 @@ import User from "../model/userModel.js";
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-    });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 };
 
 // @desc    Register new user
 export const registerUser = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-        if (!name || !email || !password) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        const userExists = await User.findOne({ email });
-        if (userExists) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        const user = await User.create({ name, email, password });
-
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user._id),
-        });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // @desc    Login user
 export const loginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-        if (!user || !(await user.matchPassword(password))) {
-            return res.status(401).json({ message: "Invalid credentials" });
-        }
-
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user._id),
-        });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-};
 
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+
+    console.log("Login attempt:", email, password);
+    console.log("Found user:", user ? user.email : "No user");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const blacklist = new Set();
 
